68ba782ad89109837af294586fb55ee38037b7ae
Pedro Rissato <peh.rissato@gmail.com>
2026-04-24 22:02:46 -0300

    feat: add Product model with BigDecimal price and discount

---
+++ src/main/java/br/com/rissato/model/Product.java
@@ -0,0 +1,86 @@
+package br.com.rissato.model;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    private Integer stock;
+    private String description;
+    private BigDecimal discountPercentage = BigDecimal.ZERO;
+
+    public Product(String name, Long id, BigDecimal price, Integer stock) {
+        this(name, id, price, stock, null);
+    }
+
+    public Product(String name, Long id, BigDecimal price, Integer stock, String description) {
+        this.name = name;
+        this.id = id;
+        this.price = price;
+        this.stock = stock;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public Long getId() {
+        return id;
+    }
+    public BigDecimal getPrice() {
+        return price;
+    }
+    public Integer getStock() {
+        return stock;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public BigDecimal getDiscountPercentage() {
+        return discountPercentage;
+    }
+
+
+   public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStock(Integer quantity) {
+        this.stock = quantity;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+   
+
+
+    public void setDiscountPercentage(BigDecimal discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product)) return false;
+        Product product = (Product) o;
+        return java.util.Objects.equals(id, product.id);
+
+    }
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(id);
+    }
+    @Override
+    public String toString(){
+        return "ID: " + id + " | Name: " + name + " | Price: " + price + " | Stock: " + stock + " | Description: " + description + " | Discount Percentage: " + discountPercentage + "%";
+    }
+
+
+}
